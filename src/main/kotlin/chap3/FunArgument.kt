345e3e85b6939a2c7ec567d21990b2c7795ee2fd
= <redalsdn7@naver.com>
2022-04-01 07:42:39 +0900

    고차함수, 람다식 (1)

---
+++ src/main/kotlin/chap3/FunArgument.kt
@@ -0,0 +1,22 @@
+package chap3
+
+// 함수의 표현
+
+// return 문이 1개 일 경우 반환 타입을({}는 = 으로 수정)생략 할 수 있다.
+fun sum(a: Int, b: Int) = a + b
+
+fun mul(a: Int, b: Int): Int {
+    return a * b
+}
+
+// 함수의 반환타입이 함수????
+fun funcFun(a: Int, b: Int) = sum(a, b)
+
+fun main() {
+
+    val result = sum(10, 10)
+    val result2 = mul(sum(10, 5), 10)
+    val result3 = funcFun(2, 3)
+
+    println("result: $result, result2: $result2, result3: $result3")
+}
